cb2ca3e18c136814a8c18d460f7d87839c83fca8
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-04-30 23:30:50 +0200

    Remove AddFilter method from ILogSink and IAsyncLogSink interfaces

--- src/LogSinks/IAsyncLogSink.cs
+++ src/LogSinks/IAsyncLogSink.cs
@@ -13,15 +13,6 @@ public interface IAsyncLogSink
     // └─────────────────────────────────────────────────────────────────────────────┘
 
     /// <summary>
-    /// Adds the <paramref name="filter"/> to this log sink. The filter will be used 
-    /// to determine whether a log message should be processed by this log sink or not.
-    /// </summary>
-    /// <param name="filter">The filter to add.</param>
-    /// <returns>A <see cref="IDisposable"/> that, when disposed, removes the filter from the log sink.</returns>
-    public IDisposable AddFilter<TPayload>(LogMessageFilter<TPayload> filter)
-        where TPayload : notnull;
-
-    /// <summary>
     /// Submits a <see cref="ILogMessage{TPayload}"/> to this log sink for processing.
     /// </summary>
     /// <param name="logMessage">The <see cref="ILogMessage{TPayload}"/> to submit.</param>
